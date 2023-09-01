8a96dab893c4499d991092ac48d4521f87d6126b
Aloshychev Ihor <hiranyagarbha@i.ua>
2023-09-01 16:53:39 +0300

    added swagger

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restful import Api
 import config
-from src.routes import Smoke, FilmListApi
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
+
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
@@ -11,7 +12,19 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
 
-api.add_resource(Smoke, '/smoke', strict_slashes=False)
-api.add_resource(FilmListApi, '/films', '/films/<uuid>', strict_slashes=False)
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Flask tutorial'
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 from src import models
+from src.routes import Smoke, FilmListApi
+
+api.add_resource(Smoke, '/smoke', strict_slashes=False)
+api.add_resource(FilmListApi, '/films', '/films/<uuid>', strict_slashes=False)
--- src/routes.py
+++ src/routes.py
@@ -1,39 +1,100 @@
+from datetime import datetime
+from flask import request
+from src import db
 from flask_restful import Resource
-
-def get_all_films():
-    pass
-
-def get_film_by_uuid(uuid:str) -> dict:
-    films = get_all_films()
-    film = list(filter(lambda f: f['id'] == uuid, films))
-    return film[0] if film else {}
-
-def create_film(film_json: dict):
-    films = get_all_films()
-    films.append(film_json)
-    return films
+from src.models import Film
 
 class Smoke(Resource):
     def get(self):
         return {'message': 'ok'}, 200
 
 class FilmListApi(Resource):
+
     def get(self, uuid=None):
         if not uuid:
-            films = get_all_films()
-        film = get_film_by_uuid(uuid)
+            films = db.session.query(Film).all()
+            return [f.to_dict() for f in films], 200
+        film = db.session.query(Film).filter_by(uuid=uuid).first()
         if not film:
             return '', 404
-        return film, 200
+        return film.to_dict(), 200
 
     def post(self):
-        pass
-    def put(self):
-        pass
-    def patch(self):
-        pass
-    def delete(self):
-        pass
+        film_json = request.json
+        if not film_json:
+            return {'message': 'Wrong data'}, 400
+        try:
+            film = Film(
+                title=film_json['title'],
+                release_date=datetime.strptime(film_json['release_date'], '%B %d, %Y'),
+                distributed_by=film_json['distributed_by'],
+                rating=film_json.get('rating'),
+                length=film_json.get('length'),
+                description=film_json.get('description')
+            )
+            db.session.add(film)
+            db.session.commit()
+        except(ValueError, KeyError) as e:
+            return f"{e}", 400
+        return {'message': 'Created successfully'}, 201
+
+    def put(self, uuid):
+        film_json = request.json
+        if not film_json:
+            return {'message': 'Wrong data'}, 400
+        try:
+            db.session.query(Film).filter_by(uuid=uuid).update(
+                dict(
+                    title=film_json['title'],
+                    release_date=datetime.strptime(film_json['release_date'], '%B %d, %Y'),
+                    distributed_by=film_json['distributed_by'],
+                    rating=film_json.get('rating'),
+                    length=film_json.get('length'),
+                    description=film_json.get('description')
+                )
+            )
+            db.session.commit()
+        except (ValueError, KeyError) as e:
+            return f'{e}', 400
+        return {'message': 'Updated successfully'}, 200
+
+    def patch(self, uuid):
+        film = db.session.query(Film).filter_by(uuid=uuid).first()
+        if not film:
+            return "", 404
+        film_json = request.json
+        title = film_json.get('title')
+        release_date = datetime.strptime(film_json.get('release_date'), '%B %d, %Y') if film_json.get(
+            'release_date') else None
+        distributed_by = film_json.get('distributed_by'),
+        rating = film_json.get('rating'),
+        length = film_json.get('length'),
+        description = film_json.get('description')
+
+        if title:
+            film.title = title
+        elif release_date:
+            film.release_date = release_date
+        elif distributed_by:
+            film.distributed_by = distributed_by
+        elif rating:
+            film.rating = rating
+        elif length:
+            film.length = length
+        elif description:
+            film.description = description
+
+        db.session.add(film)
+        db.session.commit()
+        return {'message': 'Updated successfully'}, 200
+
+    def delete(self, uuid):
+        film = db.session.query(Film).filter_by(uuid=uuid).first()
+        if not film:
+            return "", 404
+        db.session.delete(film)
+        db.session.commit()
+        return '', 204
 
 class ActorListApi(Resource):
     def get(self, uuid=None):
@@ -52,3 +113,4 @@ class ActorListApi(Resource):
         pass
     def delete(self):
         pass
+
